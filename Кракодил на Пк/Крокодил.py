a3099a1388a4fe88223a3724f30533797eef384f
ILKa123 <124565694+ILKa123@users.noreply.github.com>
2024-01-24 11:09:24 +0200

    Add files via upload

---
+++ Кракодил на Пк/Крокодил.py
@@ -0,0 +1,12 @@
+number = input("Choose a number (1-10): ")
+import random
+lst = [1,2,3,4,5,6,7,8,9,10]
+a = random. choice(lst)
+print(a) 
+if number == a :
+    print("TY PROIGRAL HAHAHA") 
+
+else: 
+    print("TY KRASAVCHIK")
+
+input()
